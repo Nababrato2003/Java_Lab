b83fcbcce5ec4dd1a39854933ea411397f3187d2
Nababrato2003 <158729490+Nababrato2003@users.noreply.github.com>
2025-03-16 21:24:14 +0530

    Add files via upload

---
+++ ass3.java
@@ -0,0 +1,14 @@
+public  class ass3 {
+    public static void main(String[] args) {
+        double radius = 7.5;
+        double pi = 3.14; 
+        
+        double perimeter = 2 * pi * radius;
+        double area = pi * radius * radius;
+        
+        // Print the results
+        System.out.println("Perimeter is = " + perimeter);
+        System.out.println("Area is = " + area);
+    }
+}
+
